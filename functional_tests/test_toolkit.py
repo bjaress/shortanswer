d9c7680ab9d40b02b03139b8468e63d826987cfb
Brian Jaress <bjaress@gmail.com>
2014-09-27 23:02:14 -1000

    Failing test for broken stylesheets.

--- functional_tests/test_toolkit.py
+++ functional_tests/test_toolkit.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import unittest
+from urllib import request
 
 SERVER='http://localhost:5000'
 
@@ -30,10 +31,35 @@ class ToolIntegrationTest(unittest.TestCase):
 
         """
         self.browser.get(SERVER)
-        scripts = self.browser.find_elements_by_tag_name('script')
+        compressed = [element.get_attribute('src')
+                for element in self.browser.find_elements_by_tag_name('script')
+                if 'CACHE' in element.get_attribute('src')]
+        self.assertTrue(len(compressed) > 0,
+                msg="At least some scripts should be compressed.")
+        for url in compressed:
+            self.follow(url)
 
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('Scripts working', header_text)
+    def test_styles(self):
+        """Check that the style-sheets are hooked up and the compressor
+        isn't choking.
+
+        """
+        self.browser.get(SERVER)
+        compressed = [element.get_attribute('href')
+                for element in self.browser.find_elements_by_tag_name('link')
+                if element.get_attribute('rel') == 'stylesheet'
+                    and 'CACHE' in element.get_attribute('href')]
+        self.assertTrue(len(compressed) > 0,
+                msg="At least some style-sheets should be compressed.")
+        for url in compressed:
+            self.follow(url)
+
+    def follow(self, url):
+        self.browser.get(url)
+        result = request.urlopen(url)
+        self.assertTrue(len(result.read()) > 0,
+                msg="URL should lead somewhere.")
+        return True
 
 if __name__ == '__main__':
     unittest.main()
