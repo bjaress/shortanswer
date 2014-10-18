78f2aeef184df5bcee7bb93f2a589364da0d0aa4
Brian Jaress <bjaress@gmail.com>
2014-10-18 12:56:03 -1000

    Minimal question form outline.

--- shortanswer/urls.py
+++ shortanswer/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'shortapp.views.home_page', name='home'),
+    url(r'^question', 'shortapp.views.question'),
     # Examples:
     # url(r'^$', 'shortanswer.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
--- shortapp/views.py
+++ shortapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request, 'shortapp/home.html')
+
+def question(request):
+    return render(request, 'shortapp/question_form.html')
