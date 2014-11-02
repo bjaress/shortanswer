8687ef688498f2693c4afb8f8b8224f4cce8c418
Brian Jaress <bjaress@gmail.com>
2014-11-01 17:09:35 -1000

    Fix broken change.

--- shortapp/models.py
+++ shortapp/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.forms import ModelForm
 
 class Question(models.Model):
-    text = models.TextField(verbose_name="question text")
+    text = models.TextField(
+            verbose_name="question text",
+            max_length="3000")
 
 class QuestionForm(ModelForm):
     class Meta:
